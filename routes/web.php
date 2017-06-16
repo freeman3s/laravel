16ca655089a845a32084bb28ae4a229772d8dd2b
freeman3s <ss298926@gmail.com>
2017-06-16 23:33:17 +0300

    Now I can save posts in the database. Finished eleventh lesson.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@
 Route::get('/about', function () {
 	return view('about');
 });
+Route::get('/', 'PostsController@index');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
-Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
