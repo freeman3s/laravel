11db07f5a85a4d10193a726a792ab535477e185a
freeman3s <ss298926@gmail.com>
2017-06-16 17:37:56 +0300

    I learned how to use migration and how to display data from the database.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
 Route::get('/about', function () {
 	return view('about');
 });
+
+Route::get('/tasks', function () {
+	$tasks = DB::table('tasks')->get();
+	return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+	$task = DB::table('tasks')->find($id);
+	return view('tasks.show', compact('task'));
+});
