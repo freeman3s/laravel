acb1a48719c28f2351d7562e496598a9735c3995
freeman3s <ss298926@gmail.com>
2017-06-16 18:49:05 +0300

    I created and worked with Task model. Finished seven lesson.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\Task;
+
 Route::get('/', function () {
 	return view('welcome');
 });
@@ -20,11 +22,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/tasks', function () {
-	$tasks = DB::table('tasks')->get();
+	$tasks = Task::all();
 	return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$task = DB::table('tasks')->find($id);
+	$task = Task::find($id);
 	return view('tasks.show', compact('task'));
 });
