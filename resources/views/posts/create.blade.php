b7d074b952c66394cc413512ceeb2b4dca93952d
freeman3s <ss298926@gmail.com>
2017-06-16 23:55:00 +0300

    Added simple form validation. Finished twelfth lesson.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,15 +9,19 @@
 
 			<div class="form-group">
 				<label for="title">Title</label>
-				<input type="text" class="form-control" id="title" name="title">
+				<input type="text" class="form-control" id="title" name="title" required>
 			</div>
 
 			<div class="form-group">
 				<label for="body">Body</label>
-				<textarea id="body" name="body" class="form-control"></textarea>
+				<textarea id="body" name="body" class="form-control" required></textarea>
 			</div>
 
-			<button type="submit" class="btn btn-primary">Publish</button>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Publish</button>
+			</div>
+
+			@include ('layouts.errors')
 		</form>
 	</div>
 @endsection
