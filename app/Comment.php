117a9ea3fdc07d19716f8af0dbbeb5315b244237
freeman3s <ss298926@gmail.com>
2017-06-17 11:25:21 +0300

    Added form to create a comment. Finished sixteenth lesson.

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+	protected $fillable = ['body', 'post_id'];
+
     public function post()
     {
     	$this->belongsTo(Post::class);
