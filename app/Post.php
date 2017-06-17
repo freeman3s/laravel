4f621bbcf37fe902d071f7c52ffbcb44404870af
freeman3s <ss298926@gmail.com>
2017-06-17 23:18:40 +0300

    Created archives. Finished twentieth lesson.

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -25,5 +26,16 @@ class Post extends Model
 			'post_id' => $this->id,
 			'user_id' => auth()->id()
 		]);
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+		if ($month = $filters['month']) {
+			$query->whereMonth('created_at', Carbon::parse($month)->month);
+		}
+
+		if ($year = $filters['year']) {
+			$query->whereYear('created_at', $year);
+		}
     }
 }
