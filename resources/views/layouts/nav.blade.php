3559a4acd81d8f0b6c40010d0c8987ec50fb0612
freeman3s <ss298926@gmail.com>
2017-06-19 00:24:14 +0300

    Added drop down list in menu for authenticated users.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,24 @@
 <div class="blog-masthead">
 	<div class="container">
 		<nav class="nav blog-nav">
-			<a class="nav-link active" href="#">Home</a>
+			<a class="nav-link active" href="/">Home</a>
 			<a class="nav-link" href="#">New features</a>
 			<a class="nav-link" href="#">Press</a>
 			<a class="nav-link" href="#">New hires</a>
 			@if (Auth::check())
-				<a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+				<li class="nav-item dropdown ml-auto">
+					<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+					<div class="dropdown-menu" aria-labelledby="dropdown04">
+						<a class="dropdown-item" href="/posts/create">Add Post</a>
+						<a class="dropdown-item" href="/logout">Logout</a>
+						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Another action</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+			@else
+				<a class="nav-link ml-auto" href="/register">Sign Up</a>
+				<a class="nav-link" href="/login">Sign In</a>
 			@endif
 		</nav>
 	</div>
